0d6aa502c26caa8c257ac8948410b80b281ab2bf
Adhri14 <adhri.adly@gmail.com>
2022-02-12 19:14:04 +0800

    CRUD Voucher

---
+++ app/voucher/router.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const {
+  index,
+  viewCreate,
+  actionCreate,
+  actionDelete,
+  viewEdit,
+} = require("./controller");
+const router = express.Router();
+const multer = require("multer");
+const os = require("os");
+
+router.get("/", index);
+router.get("/create", viewCreate);
+router.post(
+  "/create",
+  multer({ dest: os.tmpdir() }).single("image"),
+  actionCreate
+);
+router.delete("/delete/:id", actionDelete);
+router.get("/edit/:id", viewEdit);
+
+module.exports = router;
